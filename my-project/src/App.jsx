a6397d76ec9f176137dac1a9b081ab73ef756b10
shahinchowdory02 <shaheenchowdory80250@gmail.com>
2025-11-27 07:21:40 +0600

    27 11 2025

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,7 +4,7 @@ import { CiBookmarkCheck } from "react-icons/ci";
 const App = () => {
   return (
     <div className="parent h-screen w-screen bg-[#111] p-20">
-      <div className="card  w-80 h-96 bg-white rounded-2xl shadow-lg p-10">
+      <div className="card w-80 h-96 bg-white rounded-2xl shadow-lg p-10">
         <div className="top flex justify-between items-center align-middle">
           <img
             src={img}
@@ -20,17 +20,24 @@ const App = () => {
         </div>
 
         <div className="center">
-          <h3>
-            Amazon <span>5 days ago</span>
+          <h3 className="text-[15px] font-normal mt-4 mb-1 gap-2">
+            Amazon
+            <span className="text-[10px] font-normal text-[#8b8b8b]">
+              5 days ago
+            </span>
           </h3>
-          <h2>Senior UI/UX Designer</h2>
-          <div>
-            <h4>Part Time</h4>
-            <h4>Senior Leavel</h4>
+          <h2 className="text-[20px]">Senior UI/UX Designer</h2>
+          <div className="flex align-middle space-between gap-2  mt-3 mb-3">
+            <h4 className="text-[14px] text-[#111] bg-[#dadada] py-.5 px-1.5 rounded-lg">
+              Part Time
+            </h4>
+            <h4 className="text-[14px]  text-[#111] bg-[#dadada] py-.5 px-1.5 rounded-lg">
+              Senior Leavel
+            </h4>
           </div>
         </div>
 
-        <div className="bottom flex align-middle space-between border-t-2 border-[#b1b1b1] py-2.5 px-0">
+        <div className="bottom flex align-middle space-between border-t-2 border-[#b1b1b1] pt-[15px] mt-20">
           <div>
             <h3>$120/hr</h3>
             <p>Mumbai, India</p>
