1fb8925a2d508bed2eb641a05adb5b74086dd26f
shahinchowdory02 <shaheenchowdory80250@gmail.com>
2025-12-01 22:17:13 +0600

    1 12 2026

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,51 +1,12 @@
-import img from "./assets/react.svg";
 import "./App.css";
-import { CiBookmarkCheck } from "react-icons/ci";
+import Card from "./components/Card";
+
 const App = () => {
   return (
-    <div className="parent h-screen w-screen bg-[#111] p-20">
-      <div className="card w-80 h-96 bg-white rounded-2xl shadow-lg p-10">
-        <div className="top flex justify-between items-center align-middle">
-          <img
-            src={img}
-            alt="amazon"
-            className="w-8 h-8 border-2 border-white shadow-lg rounded-xl"
-          />
-          <button className="border border-[#dadada] bg-blue-100 px-2 py-1 rounded-xl shadow-md hover:bg-blue-200 flex items-center gap-2 align-middle text-[11px] text-[b1b1b1b] ">
-            Save
-            <span>
-              <CiBookmarkCheck size={15} />
-            </span>
-          </button>
-        </div>
-
-        <div className="center">
-          <h3 className="text-[15px] font-normal mt-4 mb-1 gap-2">
-            Amazon
-            <span className="text-[10px] font-normal text-[#8b8b8b]">
-              5 days ago
-            </span>
-          </h3>
-          <h2 className="text-[20px]">Senior UI/UX Designer</h2>
-          <div className="flex align-middle space-between gap-2  mt-3 mb-3">
-            <h4 className="text-[14px] text-[#111] bg-[#dadada] py-.5 px-1.5 rounded-lg">
-              Part Time
-            </h4>
-            <h4 className="text-[14px]  text-[#111] bg-[#dadada] py-.5 px-1.5 rounded-lg">
-              Senior Leavel
-            </h4>
-          </div>
-        </div>
-
-        <div className="bottom flex align-middle space-between border-t-2 border-[#b1b1b1] pt-[15px] mt-20">
-          <div>
-            <h3>$120/hr</h3>
-            <p>Mumbai, India</p>
-          </div>
-          <button>Apply Now</button>
-          <p>Hello World</p>
-        </div>
-      </div>
+    <div className="parent flex gap-10 justify-center items-center min-h-screen bg-[#a5a5a5] p-10">
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
---
+++ my-project/src/components/Card.jsx
@@ -0,0 +1,77 @@
+import { CiBookmarkCheck } from "react-icons/ci";
+
+import img from "../assets/amazon_log.jpg";
+const Card = () => {
+  return (
+    <div>
+      <div className="card w-80 h-96 bg-white rounded-2xl shadow-lg p-10">
+        <div>
+          <div className="top flex justify-between items-center align-middle">
+            <img
+              src={img}
+              alt="amazon"
+              className="w-10 h-10 border-2 border-white shadow-lg rounded-xl"
+            />
+            <button className="border border-[#dadada] bg-blue-100 px-3 py-1.5 rounded-xl shadow-sm flex items-center gap-2 align-   transition-all duration-300 ease-out hover:bg-blue-200 middle text-[11px] text-[b1b1b1b] hover:shadow-md hover:-translate-y-[1px] Follow">
+              {" "}
+              Save
+              <span>
+                <CiBookmarkCheck size={15} />
+              </span>
+            </button>
+          </div>
+
+          <div className="center">
+            <h3 className="text-[15px] font-normal mt-4 mb-1">
+              Amazon
+              <span className="text-[10px] font-normal text-[#8b8b8b]">
+                5 days ago
+              </span>
+            </h3>
+            <h2 className="text-[20px] font-bold">Senior UI/UX Designer</h2>
+            <div className="flex align-middle space-between gap-2 mt-3 mb-3">
+              <h4
+                className="text-[11px] text-center text-[#222] bg-[#dadada] py-1.5 px-3 rounded-lg font-medium border border-[#e5e5e5] transition-all duration-300
+    hover:bg-[#e6e6e6]"
+              >
+                Part Time
+              </h4>
+              <h4
+                className="text-[12px]
+    text-[#222]
+    bg-[#f0f0f0]
+    py-1.5 px-3
+    rounded-xl
+    font-medium
+    shadow-sm
+    border border-[#e5e5e5]
+    transition-all duration-300
+    hover:bg-[#e6e6e6]"
+              >
+                Senior Leavel
+              </h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="bottom flex gap-6 border-t-[1.5px] border-[#d7d7d7ce] pt-[15px] mt-16">
+          <div>
+            <h3 className="font-bold text-2xl">$120/hr</h3>
+            <p>Mumbai, India</p>
+          </div>
+          <div>
+            <button
+              className="px-2 py-3 rounded-xl font-semibold 
+    text-[111] from-blue-600 to-indigo-600 shadow-lg shadow-blue-200 hover:shadow-xl hover:shadow-blue-300 hover:scale-[1.03]
+    active:scale-[0.97] transition-all duration-300 ease-out"
+            >
+              Apply Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
